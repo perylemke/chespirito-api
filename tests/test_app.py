41dbe7ad849304e251bd593c52b7e70541e9fa9e
Pery Lemke <pery.lemke@gmail.com>
2021-11-12 15:28:36 -0300

    Implement MongoDB

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,210 +1,169 @@
-from fastapi.testclient import TestClient
+import pytest
+from httpx import AsyncClient
+from asyncio import get_event_loop
+
 from app.main import app
 
 
-# Starts API for tests
-client = TestClient(app)
+@pytest.fixture(scope="module")
+async def async_client():
+    async with AsyncClient(app=app, base_url="http://test") as client:
+        yield client
+
+
+@pytest.fixture(scope="module")
+def event_loop():
+    loop = get_event_loop()
+    yield loop
 
 
 # Actors (Atores)
-def test_actors_status_code():
+@pytest.mark.asyncio
+async def test_get_actors_status_code(async_client: AsyncClient) -> None:
     """
-    Verifica se Status Code é 200.
+    Verifica se o Status Code é 200.
     """
-    response = client.get("/api/v1/atores/")
+    response = await async_client.get("/api/v1/atores/")
     assert response.status_code == 200
 
 
-def test_actors_response_json():
+@pytest.mark.asyncio
+async def test_get_actors_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/atores/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/atores/")
+    assert len(response.json()) > 0
 
 
 # Actresses (Atrizes)
-def test_actresses_status_code():
+@pytest.mark.asyncio
+async def test_get_actresses_status_code(async_client: AsyncClient) -> None:
     """
-    Verifica se Status Code é 200.
+    Verifica se o Status Code é 200.
     """
-    response = client.get("/api/v1/atrizes/")
+    response = await async_client.get("/api/v1/atrizes/")
     assert response.status_code == 200
 
 
-def test_actresses_response_json():
+@pytest.mark.asyncio
+async def test_get_actresses_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/atrizes/")
-    assert response.json() == {"Status": "Work in Progress"}
-
-
-# Houses (Casas)
-def test_houses_status_code():
-    """
-    Verifica se Status Code é 200.
-    """
-    response = client.get("/api/v1/casas/")
-    assert response.status_code == 200
-
-
-def test_houses_response_json():
-    """
-    Verifica se o retorno é um JSON.
-    """
-    response = client.get("/api/v1/casas/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/atrizes/")
+    assert len(response.json()) > 0
 
 
 # Voice Actresses (Dubladoras)
-def test_voice_actresses_status_code():
+@pytest.mark.asyncio
+async def test_get_voice_actresses_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/dubladoras/")
+    response = await async_client.get("/api/v1/dubladoras/")
     assert response.status_code == 200
 
 
-def test_voice_actresses_response_json():
+@pytest.mark.asyncio
+async def test_get_voice_actresses_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/dubladoras/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/dubladoras/")
+    assert len(response.json()) > 0
 
 
 # Voice Actors (Dubladores)
-def test_voice_actors_status_code():
+@pytest.mark.asyncio
+async def test_get_voice_actors_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/dubladores/")
+    response = await async_client.get("/api/v1/dubladores/")
     assert response.status_code == 200
 
 
-def test_voice_actors_response_json():
+@pytest.mark.asyncio
+async def test_get_voice_actors_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/dubladores/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/dubladores/")
+    assert len(response.json()) > 0
 
 
 # Episodes (Episódios)
-def test_episodes_status_code():
+@pytest.mark.asyncio
+async def test_get_episodes_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/episodios/")
+    response = await async_client.get("/api/v1/episodios/")
     assert response.status_code == 200
 
 
-def test_episodes_response_json():
+@pytest.mark.asyncio
+async def test_get_episodes_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/episodios/")
-    assert response.json() == {"Status": "Work in Progress"}
-
-
-# Voice Studios (Estúdios de Dublagem)
-def test_voice_studios_status_code():
-    """
-    Verifica se Status Code é 200.
-    """
-    response = client.get("/api/v1/estudios/")
-    assert response.status_code == 200
-
-
-def test_voice_studios_response_json():
-    """
-    Verifica se o retorno é um JSON.
-    """
-    response = client.get("/api/v1/estudios/")
-    assert response.json() == {"Status": "Work in Progress"}
-
-
-# Movies (Filmes)
-def test_movies_status_code():
-    """
-    Verifica se Status Code é 200.
-    """
-    response = client.get("/api/v1/filmes/")
-    assert response.status_code == 200
-
-
-def test_movies_response_json():
-    """
-    Verifica se o retorno é um JSON.
-    """
-    response = client.get("/api/v1/filmes/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/episodios/")
+    assert len(response.json()) > 0
 
 
 # Quotes (Frases)
-def test_quotes_status_code():
+@pytest.mark.asyncio
+async def test_get_quotes_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/frases/")
+    response = await async_client.get("/api/v1/frases/")
     assert response.status_code == 200
 
 
-def test_quotes_response_json():
+@pytest.mark.asyncio
+async def test_get_quotes_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/frases/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/frases/")
+    assert len(response.json()) > 0
 
 
 # Characters (Personagens)
-def test_character_status_code():
+@pytest.mark.asyncio
+async def test_get_characters_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/personagens/")
+    response = await async_client.get("/api/v1/personagens/")
     assert response.status_code == 200
 
 
-def test_character_response_json():
+@pytest.mark.asyncio
+async def test_get_characters_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/personagens/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/personagens/")
+    assert len(response.json()) > 0
 
 
 # Shows (Programas)
-def test_shows_status_code():
+@pytest.mark.asyncio
+async def test_get_shows_status_code(async_client: AsyncClient) -> None:
     """
     Verifica se Status Code é 200.
     """
-    response = client.get("/api/v1/programas/")
+    response = await async_client.get("/api/v1/programas/")
     assert response.status_code == 200
 
 
-def test_shows_response_json():
+@pytest.mark.asyncio
+async def test_get_shows_response_all(async_client: AsyncClient) -> None:
     """
-    Verifica se o retorno é um JSON.
+    Verifica se retorna um número maior que 0 do Banco de Dados.
     """
-    response = client.get("/api/v1/programas/")
-    assert response.json() == {"Status": "Work in Progress"}
-
-
-# Seasons (Temporadas)
-def test_seasons_status_code():
-    """
-    Verifica se Status Code é 200.
-    """
-    response = client.get("/api/v1/temporadas/")
-    assert response.status_code == 200
-
-
-def test_seasons_response_json():
-    """
-    Verifica se o retorno é um JSON.
-    """
-    response = client.get("/api/v1/temporadas/")
-    assert response.json() == {"Status": "Work in Progress"}
+    response = await async_client.get("/api/v1/programas/")
+    assert len(response.json()) > 0
