3beabf11465a0e7cc9355b7715d7a4648c9dc38b
Pery Lemke <pery.lemke@gmail.com>
2021-11-16 11:47:54 -0300

    Try to fix CI process

--- scripts/migrations.py
+++ scripts/migrations.py
@@ -8,30 +8,31 @@ client = MongoClient(MONGO_URI)
 
 
 def insert_data_general():
-    db = client['generaldb']
+    db_general = client['generaldb']
 
-    with open('migrations/general/actors.json') as actors_file:
+    with open('./migrations/general/actors.json') as actors_file:
         file_data = json.load(actors_file)
 
-    actors_data = db.actors.insert_many(file_data)
+    actors_data = db_general.actors.insert_many(file_data)
     print(actors_data.inserted_ids)
 
-    with open('migrations/general/actresses.json') as actresses_file:
+    with open('./migrations/general/actresses.json') as actresses_file:
         file_data = json.load(actresses_file)
 
-    actresses_data = db.actors.insert_many(file_data)
+    actresses_data = db_general.actors.insert_many(file_data)
     print(actresses_data.inserted_ids)
 
 
 def insert_data_chaves():
-    db = client['chavesdb']
+    db_chaves = client['chavesdb']
 
-    with open('migrations/general/episodes1972.json') as episode_file:
+    with open('./migrations/general/episodes1972.json') as episode_file:
         file_data = json.load(episode_file)
 
-    episodes_data = db.actors.insert_many(file_data)
+    episodes_data = db_chaves.actors.insert_many(file_data)
     print(episodes_data.inserted_ids)
 
 
 if __name__ == '__main__':
     insert_data_general()
+    insert_data_chaves()
